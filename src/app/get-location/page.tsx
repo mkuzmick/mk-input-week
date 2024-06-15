7a84f00b6c77ef1bc4fb8b5d24b3e6536e6c7531
mkuzmick <mkuzmick@gmail.com>
2024-06-15 13:54:32 -0400

    Updates:

--- src/app/get-location/page.tsx
+++ src/app/get-location/page.tsx
@@ -1,14 +1,13 @@
 import GetLocation from "@/components/GetLocation";
-import MapComponent from "@/components/MapComponent";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <GetLocation />
-      <MapComponent />
     </div>
   );
 };
 
 export default HomePage;
+
